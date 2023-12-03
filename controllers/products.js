27340a562f883ba4fa92e690131f7ddd1c8db0dc
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-03 06:58:18 -0500

    routing for / products/static and /products done

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,28 @@
+
+const Model = require('../models/product');
+
 // controller setup 
 
-
 const getAllProductsStatic = async (req,res) => { //async para usar mongo
-  throw new Error('Testing async errors')
-  res.status(200).json({msg: 'products testing route'})
+
+  //static -> yo predefino las querying conditions, no me las mandan por request
+  // en la static, muestro solo los que estan featured.
+  const products = await Model.find({
+    featured: true
+  })
+
+  // throw new Error('Testing async errors')
+
+  //number of hits para saber how much i've sended
+  res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res) => {
-  res.status(200).json({msg: 'products testing route'})
+
+  // dynaminc -> envío los querying conditions en la peticion
+  const products = await Model.find(req.query)
+
+  res.status(200).json({products, nhHits: products.length})
 }
 
 module.exports = {
--- routes/products.js
+++ routes/products.js
@@ -6,6 +6,7 @@ const express = require('express');
 const router = express.Router();
 const { getAllProducts, getAllProductsStatic } = require('../controllers/products');
 
+// routing
 router.get('/', getAllProducts);
 router.get('/static', getAllProductsStatic);
 
