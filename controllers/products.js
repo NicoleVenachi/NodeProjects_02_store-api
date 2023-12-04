9d041438acd38245cf750ef8c0ec52d4df8d1ba4
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-04 06:16:45 -0500

    Pagination Logic (limit and skip)

--- controllers/products.js
+++ controllers/products.js
@@ -16,7 +16,9 @@ const getAllProductsStatic = async (req,res) => { //async para usar mongo
   const products = await Model.
     find({}).
     sort({name: 1, price: 1}).
-    select({name:1, price: 1});
+    select({name:1, price: 1}).
+    limit(5).
+    skip(1);
 
   // throw new Error('Testing async errors')
 
@@ -27,7 +29,7 @@ const getAllProductsStatic = async (req,res) => { //async para usar mongo
 const getAllProducts = async (req,res) => {
 
   // console.log(req.query);
-  const {featured, company, name, sort, fields} = req.query; //destructuro qué voy a buscar
+  const {featured, company, name, sort, fields,} = req.query; //destructuro qué voy a buscar
   const queryObject = {} //estructuro un objeto de busquedaapropiadamente
 
   if (featured) { //if exists
@@ -61,6 +63,16 @@ const getAllProducts = async (req,res) => {
     const fieldsList = fields.split(',').join(' ')
     result = result.select(fieldsList)
   }
+
+  //pagination
+  const page = Number(req.query.page) || 1; //Sino lo pasan, voy a la 1
+  const limit = Number(req.query.limit) || 10; // Sino lo pasan, tomamos 10
+
+  const skip = (page-1)*limit//pagination logic (al inicio no hace skip de nada, en la sgte los primeros 10, etc.)
+
+  result = result.limit(limit).skip(skip);
+
+
 
   const products = await result //finally 'executing' the query
 
